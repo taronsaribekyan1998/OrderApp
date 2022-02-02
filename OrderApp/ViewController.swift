4a4b8c3179f5b6a823ef6b15ad178fa0c5dc475c
Taron <saribekyantaron1998@gmail.com>
2022-02-02 19:50:46 +0400

    Initial Commit

---
+++ OrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderApp
+//
+//  Created by Taron on 02.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
