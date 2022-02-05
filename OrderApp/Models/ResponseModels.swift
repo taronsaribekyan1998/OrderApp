973246605cad7c5bec4c1c770748c3999973454e
Taron <saribekyantaron1998@gmail.com>
2022-02-05 11:12:33 +0400

    add part 3 code

---
+++ OrderApp/Models/ResponseModels.swift
@@ -0,0 +1,24 @@
+//
+//  ResponseModels.swift
+//  OrderApp
+//
+//  Created by Taron on 02.02.22.
+//
+
+import Foundation
+
+struct MenuResponse: Codable {
+    let items: [MenuItem]
+}
+
+struct CategoriesResponse: Codable {
+    let categories: [String]
+}
+
+struct OrderResponse: Codable {
+    let prepTime: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case prepTime = "preparation_time"
+    }
+}
